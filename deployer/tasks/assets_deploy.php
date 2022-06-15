b3a34a38247da9f364369f757dfe1dd5505a8159
Mike Street <mike@liquidlight.co.uk>
2022-06-15 09:24:16 +0100

    Reverse bandstand assets deployment

--- deployer/tasks/assets_deploy.php
+++ deployer/tasks/assets_deploy.php
@@ -2,7 +2,7 @@
 
 namespace Deployer;
 
-if(get('bandstand_compile_assets', true)) {
+if(!get('bandstand_skip_compile_assets', false)) {
 	task('deploy:assets', function () {
 		upload($this->path('html/assets'), '{{release_path}}/html');
 	});
