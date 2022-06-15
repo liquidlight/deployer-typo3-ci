7f0445aabdd5588056f0f15342e8c24867ef7790
Mike Street <mike@liquidlight.co.uk>
2022-06-15 09:53:31 +0100

    Bug: Remove assets flag
    Folders are now conditional anyway

--- deployer/tasks/assets_deploy.php
+++ deployer/tasks/assets_deploy.php
@@ -2,15 +2,13 @@
 
 namespace Deployer;
 
-if(!get('bandstand_skip_deploy_webroot_assets', false)) {
-	task('deploy:assets', function () {
-		if(is_dir($this->path('html/assets'))) {
-			upload($this->path('html/assets'), '{{release_path}}/html');
-		}
-		if(is_dir($this->path('html/_assets'))) {
-			upload($this->path('html/_assets'), '{{release_path}}/html');
-		}
-	});
+task('deploy:assets', function () {
+	if(is_dir($this->path('html/assets'))) {
+		upload($this->path('html/assets'), '{{release_path}}/html');
+	}
+	if(is_dir($this->path('html/_assets'))) {
+		upload($this->path('html/_assets'), '{{release_path}}/html');
+	}
+});
 
-	after('deploy:vendors', 'deploy:assets');
-}
+after('deploy:vendors', 'deploy:assets');
