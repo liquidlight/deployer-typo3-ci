e3507ce03616deb38a8f6a49fb69a7b06011c445
Mike Street <mike@liquidlight.co.uk>
2022-06-16 14:13:45 +0100

    Chore: Lint PHP

--- deployer/tasks/typo3cms_cache_flush.php
+++ deployer/tasks/typo3cms_cache_flush.php
@@ -3,10 +3,10 @@
 namespace Deployer;
 
 task('typo3cms:cache:flush', function () {
-    $activeDir = test('[ -e {{deploy_path}}/release ]') ?
-        get('deploy_path') . '/release' :
-        get('deploy_path') . '/current';
-    run('cd ' . $activeDir . ' &&  {{bin/php}} {{bin/typo3cms}} cache:flush');
+	$activeDir = test('[ -e {{deploy_path}}/release ]') ?
+		get('deploy_path') . '/release' :
+		get('deploy_path') . '/current';
+	run('cd ' . $activeDir . ' &&  {{bin/php}} {{bin/typo3cms}} cache:flush');
 });
 
 after('cache:clear_php_http', 'typo3cms:cache:flush');
