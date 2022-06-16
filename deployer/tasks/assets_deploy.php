c2051052fe5b81fe70d128f52fdb6b6ec4344486
Mike Street <mike@liquidlight.co.uk>
2022-06-16 13:48:11 +0100

    BE: Deploy Resources/Public folder from local EXT
    Also allow extra paths to be passed in

--- deployer/tasks/assets_deploy.php
+++ deployer/tasks/assets_deploy.php
@@ -3,11 +3,28 @@
 namespace Deployer;
 
 task('deploy:assets', function () {
-	if(is_dir($this->path('html/assets'))) {
+	/**
+	 * Rsync our standard compiled assets
+	 */
+	if (is_dir($this->path('html/assets'))) {
 		upload($this->path('html/assets'), '{{release_path}}/html');
 	}
-	if(is_dir($this->path('html/_assets'))) {
-		upload($this->path('html/_assets'), '{{release_path}}/html');
+
+	/**
+	 * Rsync local resources to handle compilation
+	 */
+	$paths = glob('app/*/Resources/Public') ?? [];
+	$paths = array_merge($paths, get('bandstand_asset_paths', []));
+
+	if (count($paths)) {
+		foreach ($paths as $path) {
+			$path = ltrim($path, '/');
+			$dest = explode('/', $path);
+			array_pop($dest);
+			$dest = implode('/', $dest);
+
+			upload($this->path($path), '{{release_path}}/' . $dest);
+		}
 	}
 });
 
