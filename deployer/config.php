b4f6a214c2e317e4f8ccd953138cf36bac768535
Mike Street <mike@liquidlight.co.uk>
2022-06-27 08:28:25 +0100

    Bug: Re-add default_stage if no .env
    Allows CI to deploy

--- deployer/config.php
+++ deployer/config.php
@@ -33,6 +33,17 @@ if (isset(
 }
 
 /**
+ * default_stage
+ *
+ * If you just run "deploy" what happens?
+ *
+ * Check if file doesn't exist - e.g. deploying via CI
+ */
+if (!file_exists(getcwd() . '/.env')) {
+	set('default_stage', 'local');
+}
+
+/**
  * composer_channel
  *
  * What composer version?
