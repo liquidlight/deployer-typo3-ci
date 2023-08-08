16254a64a77c7cd0c8e68f90f031ad7fa943c503
Mike Street <mike@liquidlight.co.uk>
2023-08-08 08:48:26 +0100

    feat: Redeclare deploy task
    - Add liquid light tasks
    - Remove http cache clear

--- deployer/tasks/cache_clear_php_http.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Deployer;
-
-/**
- * Unset HTTP cache clear as it fails with HTTP auth
- */
-task('cache:clear_php_http', function () {
-});
---
+++ deployer/tasks/deploy.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Deployer;
+
+task('deploy', [
+
+	// Standard deployer task.
+	'deploy:info',
+
+	// Read more on https://github.com/sourcebroker/deployer-extended#deploy-check-lock
+	'deploy:check_lock',
+
+	// Read more on https://github.com/sourcebroker/deployer-extended#deploy-check-composer-install
+	'deploy:check_composer_install',
+
+	// Read more on https://github.com/sourcebroker/deployer-extended#deploy-check-branch-local
+	'deploy:check_branch_local',
+
+	// Read more on https://github.com/sourcebroker/deployer-extended#deploy-check-branch
+	'deploy:check_branch',
+
+	// Standard deployer task.
+	'deploy:check_remote',
+
+	// Set variables and other pre-deploy tasks
+	'environment:prepare',
+
+	// Standard deployer task.
+	'deploy:setup',
+
+	// Standard deployer task.
+	'deploy:lock',
+
+	// Standard deployer task.
+	'deploy:release',
+
+	// Standard deployer task.
+	'deploy:update_code',
+
+	// Standard deployer task.
+	'deploy:shared',
+
+	// Standard deployer task.
+	'deploy:writable',
+
+	// Standard deployer task.
+	'deploy:vendors',
+
+	// Upload front-end assets
+	// Read more on https://gitlab.lldev.co.uk/packages/typo3/deployer#assets-upload
+	'deploy:assets',
+
+	// Standard deployer task.
+	'deploy:clear_paths',
+
+	// Create database backup, compress and copy to database store.
+	// Read more on https://github.com/sourcebroker/deployer-extended-database#db-backup
+	'db:backup',
+
+	// Start buffering http requests. No frontend access possible from now.
+	// Read more on https://github.com/sourcebroker/deployer-extended#buffer-start
+	'buffer:start',
+
+	// Truncate caching tables, all cf_* tables
+	// Read more on https://github.com/sourcebroker/deployer-extended-database#db-truncate
+	'db:truncate',
+
+	// Update database schema for TYPO3. Task from typo3_console extension.
+	'typo3cms:database:updateschema',
+
+	// Standard deployer task.
+	'deploy:symlink',
+
+	// Clear php cli cache.
+	// Read more on https://github.com/sourcebroker/deployer-extended#cache-clear-php-cli
+	'cache:clear_php_cli',
+
+	// Clear TYPO3 caches
+	'typo3cms:cache:flush',
+
+	// Frontend access possible again from now
+	// Read more on https://github.com/sourcebroker/deployer-extended#buffer-stop
+	'buffer:stop',
+
+	// Carry out any post-deploy tasks
+	// Read more on https://gitlab.lldev.co.uk/packages/typo3/deployer#set-environment
+	'environment:post-deploy',
+
+	// Standard deployer task.
+	'deploy:writable',
+
+	// Standard deployer task.
+	'deploy:unlock',
+
+	// Standard deployer task.
+	'deploy:cleanup',
+
+	// Standard deployer task.
+	'deploy:success',
+
+]);
